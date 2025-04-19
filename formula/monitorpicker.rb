9d3fd7bd62f3f337600141aaaa21a0276cabdfb6
Caesar Cavales <c.cavales3@gmail.com>
2025-04-19 15:08:13 -0400

    added zip file

--- formula/monitorpicker.rb
+++ formula/monitorpicker.rb
@@ -2,15 +2,17 @@ class Monitorpicker < Formula
   desc "Lightweight monitor picker for macOS using Hammerspoon"
   homepage "https://github.com/ccavales3/homebrew-monitorpicker"
   url "https://github.com/ccavales3/homebrew-monitorpicker/raw/main/monitorpicker.zip"
-  sha256 "bfb74f840adf94b96c265dfaac89902afafdce06a8398b18c30f5376769a9696"
+  sha256 "5870af8dc927edb1a28f0c6315e11dd681b0134dee5e0529056461d2904bb744"
   version "1.0.0"
 
   def install
-    hammerspoon_path = File.expand_path("~/.hammerspoon")
-    mkdir_p hammerspoon_path
-    cp_r "monitorpicker", "#{hammerspoon_path}/monitorpicker"
-    ohai "MonitorPicker installed to ~/.hammerspoon/monitorpicker"
-    puts "Add the following line to your ~/.hammerspoon/init.lua:"
-    puts "require('monitorpicker')"
+    hsp_dir = File.expand_path("~/.hammerspoon")
+    mkdir_p hsp_dir
+    cp_r "monitorpicker", "#{hsp_dir}/monitorpicker"
+
+    ohai "✅ MonitorPicker installed to ~/.hammerspoon/monitorpicker"
+    puts  "👉 Add the following line to your ~/.hammerspoon/init.lua if you haven't already:"
+    puts  "   require(\"monitorpicker\")"
+    puts  "Then reload Hammerspoon."
   end
 end
