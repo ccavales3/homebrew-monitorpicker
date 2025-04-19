4097d9d5fd616b95e396a157b1823f4847f850a1
Caesar Cavales <c.cavales3@gmail.com>
2025-04-19 16:24:39 -0400

    fixed sha

--- Formula/monitorpicker.rb
+++ Formula/monitorpicker.rb
@@ -4,18 +4,25 @@ class Monitorpicker < Formula
   url "https://github.com/ccavales3/homebrew-monitorpicker/raw/main/monitorpicker.zip"
   sha256 "19044ed0fce11b3c33f1820b3eb510766ae9274f83ac12c420c9c0584c853c2e"
   version "1.0.0"
+
   keg_only "this formula installs configuration files directly to ~/.hammerspoon"
 
   def install
+    # Save the files inside the Homebrew prefix so they persist through install
+    prefix.install "init.lua"
+    prefix.install "monitor_picker.lua"
+  end
+
+  def post_install
     user_home = ENV["HOME"]
     hsp_dir = File.join(user_home, ".hammerspoon", "monitorpicker")
     mkdir_p hsp_dir
 
-    cp "init.lua", "#{hsp_dir}/init.lua"
-    cp "monitor_picker.lua", "#{hsp_dir}/monitor_picker.lua"
+    cp prefix/"init.lua", "#{hsp_dir}/init.lua"
+    cp prefix/"monitor_picker.lua", "#{hsp_dir}/monitor_picker.lua"
 
-    ohai "✅ MonitorPicker installed to ~/.hammerspoon/monitorpicker"
-    puts  "👉 Add the following line to your ~/.hammerspoon/init.lua if you haven't already:"
+    ohai "✅ MonitorPicker installed to #{hsp_dir}"
+    puts  "👉 Add this to your ~/.hammerspoon/init.lua if not already present:"
     puts  "   require(\"monitorpicker\")"
     puts  "Then reload Hammerspoon."
   end
